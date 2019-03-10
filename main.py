fb49061c231f94e82bc46251e222ab6fa57291b8
pkmitl205 <58030205@kmitl.ac.th>
2019-03-10 23:19:20 +0700

    Create main.ui and main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QApplication, QDialog
+from PyQt5.uic import loadUi
+
+class main(QDialog):
+    def __init__(self):
+        super(main, self).__init__()
+        loadUi('main.ui', self)
+        self.setWindowTitle('pyQt5 GUI')
+        self.pushButton.clicked.connect(self.on_pushButton_clicked)
+    
+    @pyqtSlot()
+    def on_pushButton_clicked(self):
+        self.label_2.setText('Welcome : '+self.lineEdit.text())
+
+app=QApplication(sys.argv)
+widget=main()
+widget.show()
+sys.exit(app.exec_())
